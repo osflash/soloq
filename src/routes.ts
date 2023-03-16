71e85d9f73643faa83c781fb52a40ec80b5aaaeb
OsFlash <felipedasilvatomaz1996@gmail.com>
2023-03-16 19:01:25 -0300

    add app url

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,11 @@ import { FastifyInstance } from 'fastify'
 
 import { client } from '~/discord'
 import { prisma } from '~/services/prisma'
+import { env } from './config'
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.get('/', async () => {
-    return { status: client.isReady() }
+    return { status: client.isReady(), appUrl: env.discord.appUrl }
   })
 
   app.get('/guilds', async () => {
